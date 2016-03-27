b9b56f56584d150c64c88becd121b2cab3bc197e
adriana <adrianamolello@gmail.com>
2016-03-27 03:00:19 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Animals::Application.config.secret_key_base = '4066d95a7de1f6383fb1092a92a0be493a31e675ec1af728ba475025747c9d3b60c7a9357501f970575c1e2191c8f92e4ab7a1975ac9d46337a2327946fa7c7c'
